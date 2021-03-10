85da491911f7146874e680c9f1d2c7c256461e41
Mert Akpınar <mert.akpinar@hotmail.com>
2021-03-10 21:38:13 +0300

    second commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js'
 const router = express.Router()
 import { protect } from '../middleware/authMiddleware.js'
 
-router.route('/').get(registerUser)
+router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
-
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import {BrowserRouter as Router,Route} from 'react-router-dom'
-import {Container} from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
 import CartScreen from './Screens/CartScreen'
+import LoginScreen from './Screens/LoginScreen'
+import RegisterScreen from './Screens/RegisterScreen'
+import ProfileScreen from './Screens/ProfileScreen'
 
-const App = ()=> {
+const App = () => {
   return (
     <Router>
-    <Header/>
-    <main className="py-3">
-      <Container>
-        <Route path='/' component={HomeScreen} exact />
-        <Route path='/product/:id' component={ProductScreen} />
-        <Route path='/cart/:id?' component={CartScreen} />
-      </Container>
-    </main>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
